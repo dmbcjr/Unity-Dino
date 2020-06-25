c3ee7dda49e8b943c98d9a8855d6cfbefa44c8e9
dmbcjr <dcurranjr@gmail.com>
2020-06-25 15:40:31 +0100

    Basic Prototype
    -Player moves right consistently
    -Player can jump
    -Basic Desert Scene w/Cacti obstacles
    -Camera Following Player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    public float smoothFollow = 1f;
+    public Vector3 offset;
+
+    Vector3 endPos;
+
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+
+
+    void LateUpdate()
+    {
+        endPos = new Vector3(player.position.x, transform.position.y, transform.position.z) + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, endPos, smoothFollow);
+
+        transform.position = smoothedPosition;
+
+    }
+
+    void Update()
+    {
+        if (player.position.x >= 81)
+        {
+            player.position = new Vector3(player.position.x - 87f, player.position.y, player.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float playerSpeed = 6f;
+    Rigidbody rb;
+    [SerializeField] float jumpForce = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y == .75f)
+        {
+            rb.AddForce(transform.up * jumpForce);
+            //Debug.Log("yes");
+        }
+
+        transform.position += Vector3.right.normalized * Time.deltaTime * playerSpeed;
+
+    }
+    
+    //add 
+}
