ba9eef20e1a376c77fed231b13c6c78c2051a23c
dmbcjr <dcurranjr@gmail.com>
2020-06-29 19:48:34 +0100

    Basic Game Loop
    -Added Score & Hi Score UI
    -Added Restart menu of failure
    -Player speed increases based on distance

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI hiScore_Text;
+    public TextMeshProUGUI currentScore_Text;
+    public Transform playerDistance;
+
+    float hiScore, cScore;
+
+
+    void Start()
+    {
+        
+
+        hiScore = 0;
+        cScore = 0;
+    }
+
+    void Update()
+    {
+        TrackScore();    
+    }
+
+    private void TrackScore()
+    {
+        cScore = playerDistance.position.x;
+
+        SetCurrentScore();
+
+       
+    }
+
+    private void SetCurrentScore ()
+    {
+
+        currentScore_Text.text = Mathf.Round(cScore).ToString("00000");
+        
+       
+    }
+    
+    public void SetHighScore ()
+    {
+        if (cScore > hiScore)
+        {
+            hiScore = cScore;
+            hiScore_Text.text = Mathf.Round(cScore).ToString("00000");
+        }
+        
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,69 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float playerSpeed = 6f;
+
     Rigidbody rb;
+
     [SerializeField] float jumpForce = 1f;
+
+    bool isAlive;
+    Vector3 playerStartPos;
+
+    [SerializeField] GameObject restartMenu;
     // Start is called before the first frame update
     void Start()
     {
+        playerStartPos = transform.position;
         rb = GetComponent<Rigidbody>();
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y == .75f)
+       
+
+        if (isAlive)
         {
-            rb.AddForce(transform.up * jumpForce);
-            //Debug.Log("yes");
+
+           
+
+            if (Input.GetKeyDown(KeyCode.Space) && transform.position.y == .75f)
+            {
+                rb.AddForce(transform.up * jumpForce);
+                //Debug.Log("yes");
+            }
+            else
+            {
+                transform.position += Vector3.right.normalized * Time.deltaTime * playerSpeed;
+            }
         }
 
-        transform.position += Vector3.right.normalized * Time.deltaTime * playerSpeed;
+        playerSpeed += transform.position.x * Time.deltaTime /100;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            Time.timeScale = 0;
+            isAlive = false;
+            restartMenu.SetActive(true);
+            
+        }
+    }
+
+    public void RestartGame()
+    {
+        restartMenu.SetActive(false);
+        transform.position = playerStartPos;
+        Time.timeScale = 1;
+        isAlive = true;
+        playerSpeed = 6f;
+
 
     }
-    
+
     //add 
 }
